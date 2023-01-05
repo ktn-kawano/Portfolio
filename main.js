faaffcd80158e077aaedc71db82938e53052a86c
ktn-kawano <kotono.kawano.kk@gmail.com>
2023-01-05 17:30:09 +0900

    JSコードの見直し

--- main.js
+++ main.js
@@ -1,103 +1,46 @@
-$(function(){
-    $(".menu_about").click(function(){
-        let adjust = 180;
-        let move = $("#about").offset().top-adjust;
-        $(window).scrollTop(move);
-    })
-})
+function move_page(point){
+    let adjust = 180;
+    let move = $(point).offset().top-adjust;
+    $(window).scrollTop(move);
+}
 
-$(function(){
-    $(".menu_history").click(function(){
-        let adjust = 180;
-        let move = $("#history").offset().top-adjust;
-        $(window).scrollTop(move);
-    })
-})
+function content_on(selecters){
+    selecters.map(selecter =>{
+        document.querySelector(selecter).classList.remove('off');
+    });
+}
 
-$(function(){
-    $(".menu_skills").click(function(){
-        let adjust = 180;
-        let move = $("#skills").offset().top-adjust;
-        $(window).scrollTop(move);
-    })
-})
+function class_off(selecters){
+    selecters.map(selecter =>{
+        document.querySelector(selecter).classList.add('off');
+    });
+}
 
-$(function(){
-    $(".menu_works").click(function(){
-        let adjust = 180;
-        let move = $("#works").offset().top-adjust;
-        $(window).scrollTop(move);
-    })
-})
+function toggle_contents(show_selecter){
+    class_off(["#birthday","#birthplace","#favorite","#hobby"]);
+    content_on(["#select_content",show_selecter]);
+}
 
-$(function(){
-    $(".menu_github").click(function(){
-        let adjust = 180;
-        let move = $("#github").offset().top-adjust;
-        $(window).scrollTop(move);
-    })
-})
-
-function birthday_on(){
-    document.querySelector("#select_content").classList.remove('off');
-    document.querySelector("#birthday").classList.remove('off');
-    document.querySelector("#birthplace").classList.add('off');
-    document.querySelector("#favorite").classList.add('off');
-    document.querySelector("#hobby").classList.add('off');
-};
-
-function birthplace_on(){
-    document.querySelector("#select_content").classList.remove('off');
-    document.querySelector("#birthplace").classList.remove('off');
-    document.querySelector("#birthday").classList.add('off');
-    document.querySelector("#favorite").classList.add('off');
-    document.querySelector("#hobby").classList.add('off');
-};
-
-function favorite_on(){
-    document.querySelector("#select_content").classList.remove('off');
-    document.querySelector("#favorite").classList.remove('off');
-    document.querySelector("#birthday").classList.add('off');
-    document.querySelector("#birthplace").classList.add('off');
-    document.querySelector("#hobby").classList.add('off');
-};
-
-function hobby_on(){
-    document.querySelector("#select_content").classList.remove('off');
-    document.querySelector("#hobby").classList.remove('off');
-    document.querySelector("#birthday").classList.add('off');
-    document.querySelector("#birthplace").classList.add('off');
-    document.querySelector("#favorite").classList.add('off');
-};
-
-function move_marusho(){
-    if(confirm("株式会社丸昭のサイトへ移動します。よろしいですか？")){
-        location.href="https://www.hikakuseizo-marusho.co.jp/"
-    } else {
-        ;
+function move_site(site_title,link){
+    if(confirm(site_title+"へ移動します。よろしいですか？")){
+        location.href=link
     }
 }
 
-function move_playing(){
-    if(confirm("株式会社Playingのサイトへ移動します。よろしいですか？")){
-        location.href="https://play-ing.com/"
-    } else {
-        ;
-    }
-}
-
-const pageTop = document.querySelector(".top_btn");
-const sec = document.querySelector("#history");
-const vh = window.innerHeight;
+$(function(){
+    $(".top_btn").click(function(){
+        let move = $("#header").offset().top;
+        $(window).scrollTop(move);
+    });
+})
 
 window.addEventListener("scroll",function(){
+    const btn = document.querySelector(".top_btn");
     const scroll = window.pageYOffset;
-    const targetDistance = sec.getBoundingClientRect().top;
-    const targetPosition = scroll + targetDistance - vh;
 
-    if(targetPosition < scroll){
-        pageTop.classList.add("fade_in");
+    if(scroll > 500){
+        btn.classList.add("btn_on");
     } else {
-        pageTop.classList.remove("fade_in");
+        btn.classList.remove("btn_on");
     }
 });
